66e50ad3674daa9f290020ce57970ee20490455d
Julia <juliaborgess18@gmail.com>
2023-08-26 10:16:25 -0300

    Incrementando menu principal

--- script.sh
+++ script.sh
@@ -20,7 +20,7 @@ listar_participantes(){
 
 menu_opcoes_firewall(){
     while [ $? == 0 ];do
-        opcao=$( dialog --stdout --extra-button --extra-label "Participantes" --menu 'Digite uma opção?' 0 0 0 1 'Criar uma regra' 2 'Configurar Política Padrão' 3 'Apagar uma regra'  4 'Listar todas as regras' 5 'Apagar todas as regras' 6 'Salvar as regras do firewall' 7 'Restaurar as regras do firewall')
+        opcao=$( dialog --stdout --menu 'Digite uma opção?' 0 0 0 1 'Criar uma regra' 2 'Configurar Política Padrão' 3 'Apagar uma regra'  4 'Listar todas as regras' 5 'Apagar todas as regras' 6 'Salvar as regras do firewall' 7 'Restaurar as regras do firewall')
         case $opcao in
             1) dialog --infobox 'Que Pena!' 0 0 ;;
             2) dialog --infobox 'Que Pena!' 0 0 ;;
