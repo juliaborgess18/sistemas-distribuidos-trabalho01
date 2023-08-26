b48598f560c261447e8df1fb6d6ab769e6c9cc18
Julia <juliaborgess18@gmail.com>
2023-08-26 07:30:35 -0300

    Ajustando menu

--- script.sh
+++ script.sh
@@ -2,14 +2,16 @@
 
 listar_regras(){
     iptables_output=$(sudo iptables -L)
-    dialog --msgbox "$iptables_output" 20 60
+    dialog --msgbox "Resultado\n$iptables_output" 20 60
 }
 
 apagar_regras(){
     iptables_output=$(sudo iptables -F)
-    dialog --msgbox "$iptables_output" 20 60
+    dialog --msgbox "Resultado\n$iptables_output" 20 60
 }
+
 opcao=$( dialog --stdout --menu 'Digite uma opção?' 0 0 0 1 'Criar uma regra' 2 'Configurar Política Padrão' 3 'Apagar uma regra'  4 'Listar todas as regras' 5 'Apagar todas as regras' 6 'Salvar as regras do firewall' 7 'Restaurar as regras do firewall')
+while [ $? == 1 ];do
     case $opcao in
         1) dialog --infobox 'Você concordou!' 0 0 ;;
         2) dialog --infobox 'Que Pena!' 0 0 ;;
@@ -20,3 +22,8 @@ opcao=$( dialog --stdout --menu 'Digite uma opção?' 0 0 0 1 'Criar uma regra' 
         7) dialog --infobox 'Nem sim nem não' 0 0;;
         *) dialog --infobox 'Opção inválida!' 0 0;;
     esac
+done
+
+dialog --infobox 'Saindo do Script!' 0 0 
+exit 
+                
